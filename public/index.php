6f69d02865788cc80fd524ee6f942c4aea253997
Fabian Brash <fabianbrash@gmail.com>
2022-09-02 12:19:46 -0400

    created /user route

--- public/index.php
+++ public/index.php
@@ -7,9 +7,21 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world from the Slim framework!!");
     return $response;
+});
+
+
+$app->get('/api/v1/users', function (Request $request, Response $response, $args) {
+    
+    $data = array('name' => 'John', 'age' => 42, 'job_title' => 'Java Developer');
+    $payload = json_encode($data);
+
+    $response->getBody()->write($payload);
+    return $response
+	    ->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
